66969037b8402ac9854540b54fd832fc5d83db00
Vijet Mahabaleshwar <vijet.mahabaleshwar@okta.com>
2018-04-03 10:15:05 -0700

    Updated package-lock.json and added support for testenv

--- scripts/setup-env.js
+++ scripts/setup-env.js
@@ -18,9 +18,12 @@ const fs = require('fs');
 const { execSync } = require('child_process');
 const path = require('path');
 
+// Users can also provide the testenv configuration at the root folder
+require('dotenv').config({path: path.join(__dirname, '..', 'testenv')});
+
 function updateConfig(file) {
-  if (process.env.ISSUER === undefined || process.env.CLIENT_ID === undefined) {
-    console.error('[ERROR] Please set the ISSUER and CLIENT_ID Environment variables');
+  if (!process.env.ISSUER || !process.env.CLIENT_ID || !process.env.USERNAME || !process.env.PASSWORD) {
+    console.log('[ERROR] Please set the necessary Environment variables (ISSUER, CLIENT_ID, USERNAME, PASSWORD)');
     process.exit(1);
   }
 
@@ -33,7 +36,8 @@ function updateConfig(file) {
 function cloneRepository(repository, directory) {
   const dir = path.join(__dirname, '..', directory);
   if (fs.existsSync(dir)) {
-    console.log(`${directory} is already cloned.`);
+    console.log(`${directory} is already cloned. Getting latest version...`);
+    execSync(`cd ${directory} && git pull`)
     return;
   }
 
