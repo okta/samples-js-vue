219a204e9775478a3cb65a9b859ef21d0675e1a3
Aaron Granick <aaron.granick@okta.com>
2019-11-15 11:06:36 -0800

    polyfill TextEncoder for IE Edge

--- custom-login/src/main.js
+++ custom-login/src/main.js
@@ -13,6 +13,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import './polyfills'
 import App from './App'
 import router from './router'
 
