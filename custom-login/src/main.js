1856784691c7574fd04409ad169af2d8ef3e4de6
Matt Raible <matt.raible@okta.com>
2020-09-14 15:15:59 -0600

    Update Vue Samples to use CLI scripts and dependencies

--- custom-login/src/main.js
+++ custom-login/src/main.js
@@ -9,20 +9,11 @@
  *
  * See the License for the specific language governing permissions and limitations under the License.
  */
-
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import './polyfills'
-import App from './App'
-import router from './router'
+import App from './App.vue'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
-})
+  render: h => h(App),
+}).$mount('#app')
