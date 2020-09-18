57716168d2ce629e99abb090a4ef4457bf583961
Matt Raible <matt.raible@okta.com>
2020-09-18 12:55:19 -0600

    Add vue.config.js to configure env variables

---
+++ okta-hosted-login/vue.config.js
@@ -0,0 +1,38 @@
+// Support storing environment variables in a file named "testenv"
+const path = require('path');
+const dotenv = require('dotenv');
+const fs = require('fs');
+
+// Read environment variables from "testenv". Override environment vars if they are already set.
+const TESTENV = path.resolve(__dirname, '..', 'testenv');
+if (fs.existsSync(TESTENV)) {
+  const envConfig = dotenv.parse(fs.readFileSync(TESTENV));
+  Object.keys(envConfig).forEach((k) => {
+    process.env[k] = envConfig[k];
+  });
+}
+process.env.CLIENT_ID = process.env.CLIENT_ID || process.env.SPA_CLIENT_ID;
+
+const webpack = require('webpack');
+const env = {};
+
+// List of environment variables made available to the app
+[
+  'ISSUER',
+  'CLIENT_ID',
+].forEach(function (key) {
+  if (!process.env[key]) {
+    throw new Error(`Environment variable ${key} must be set. See README.md`);
+  }
+  env[key] = JSON.stringify(process.env[key]);
+});
+
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': env
+      })
+    ]
+  }
+}
