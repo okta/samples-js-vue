539202794140ded6920ab5077083a5087eff8054
Oleksandr Pravosudko <oleksandr.pravosudko@okta.com>
2021-01-08 20:07:04 +0000

    chore: enable e2e tests Adds the resource server start script
    OKTA-359197
    <<<Jenkins Check-In of Tested SHA: 0091f1c2e5e72c1fdd1275455fbf64581909fff7 for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: samples-js-vue

---
+++ scripts/startResourceServer.js
@@ -0,0 +1,52 @@
+/* eslint-disable consistent-return, no-console */
+
+const fs = require('fs');
+const { exec } = require('child_process');
+const path = require('path');
+const dotenv = require('dotenv');
+
+// Read environment variables from "testenv". Override environment vars if they are already set.
+const TESTENV = path.resolve(__dirname, '..', 'testenv');
+if (fs.existsSync(TESTENV)) {
+  const envConfig = dotenv.parse(fs.readFileSync(TESTENV));
+  Object.keys(envConfig).forEach((k) => {
+    process.env[k] = envConfig[k];
+  });
+}
+process.env.CLIENT_ID = process.env.CLIENT_ID || process.env.SPA_CLIENT_ID;
+
+function startResourceServer() {
+  const dir = path.join(__dirname, '..', 'samples-nodejs-express-4')
+  if (!fs.existsSync(dir)) {
+    console.error(`[ERROR] samples-nodejs-express4 doesn't exist.`);
+    return;
+  }
+
+  if (process.env.ISSUER === undefined) {
+    console.error('[ERROR] Please set the ISSUER Environment variable');
+    return;
+  }
+
+  if (process.env.CLIENT_ID === undefined) {
+    console.error('[ERROR] Please set the CLIENT_ID Environment variable');
+    return;
+  }
+
+  const command = `node ${dir}/resource-server/server.js`;
+  const options = {
+    env: {
+      ISSUER: process.env.ISSUER,
+      SPA_CLIENT_ID: process.env.CLIENT_ID,
+      PATH: process.env.PATH,
+      OKTA_TESTING_DISABLEHTTPSCHECK: process.env.OKTA_TESTING_DISABLEHTTPSCHECK,
+    }
+  };
+  exec(command, options, (err, stdout) => {
+    if (err !== null) {
+      return console.error(err);
+    }
+    return console.log(stdout);
+  });
+}
+
+startResourceServer();
