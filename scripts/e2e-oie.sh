37bfd06289fa5cb62052027b382ee2013d2dd515
Kostya <kostiantyn.drozdov@okta.com>
2021-05-17 16:16:24 +0300

    Fixed echo vars.

--- scripts/e2e-oie.sh
+++ scripts/e2e-oie.sh
@@ -38,16 +38,16 @@ function run_tests() {
     kill -s TERM $(lsof -t -i:8000 -sTCP:LISTEN)
     echo "start of envs echo"
     echo "ORG_OIE_ENABLED"
-    echo ORG_OIE_ENABLED
+    echo "${ORG_OIE_ENABLED}"
     echo "USE_INTERACTION_CODE"
-    echo USE_INTERACTION_CODE
+    echo "${USE_INTERACTION_CODE}"
     echo "end of envs echo"
     npm run test:custom-login
     echo "start of envs echo2"
     echo "ORG_OIE_ENABLED2"
-    echo ORG_OIE_ENABLED
+    echo "${ORG_OIE_ENABLED}"
     echo "USE_INTERACTION_CODE2"
-    echo USE_INTERACTION_CODE
+    echo "${USE_INTERACTION_CODE}"
     echo "end of envs echo2"
 }
 
