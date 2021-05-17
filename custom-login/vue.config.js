55eaf001453df1e71cd9a074a00e99be9a9abdc3
Kostya <kostiantyn.drozdov@okta.com>
2021-05-17 15:36:20 +0300

    Log the USE_INTERACTION_CODE and ORG_OIE_ENABLED variables.

--- custom-login/vue.config.js
+++ custom-login/vue.config.js
@@ -13,6 +13,8 @@ if (fs.existsSync(TESTENV)) {
 }
 process.env.CLIENT_ID = process.env.CLIENT_ID || process.env.SPA_CLIENT_ID;
 process.env.USE_INTERACTION_CODE = process.env.USE_INTERACTION_CODE || false;
+console.log(process.env.ORG_OIE_ENABLED);
+console.log(process.env.USE_INTERACTION_CODE);
 
 const env = {};
 
