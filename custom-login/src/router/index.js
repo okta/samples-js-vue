b1e6091db7f5d84e11c944c58020aa4d3a5a2898
Shuo Wu <wushuo2010@gmail.com>
2021-06-10 15:49:38 +0000

    feat: update samples to support vue 3 - OKTA-364845 update okta-vue to v4 Adds onAuthResume handler Updates auth-js dependency to 4.9.2 update readme to indicate vue-2 branch for vue 2 samples update lock file update root level readme to indicate vue-2 branch chore: add vue-loader-v16 to devDeps
    OKTA-364845
    <<<Jenkins Check-In of Tested SHA: 60b805c92d103c3329d84a27abbffe69f1c09bc0 for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: samples-js-vue
    Files changed count: 31
    PR Link: "https://github.com/okta/samples-js-vue/pull/104"

--- custom-login/src/router/index.js
+++ custom-login/src/router/index.js
@@ -1,41 +1,12 @@
-/*!
- * Copyright (c) 2018, Okta, Inc. and/or its affiliates. All rights reserved.
- * The Okta software accompanied by this notice is provided pursuant to the Apache License, Version 2.0 (the "License.")
- *
- * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *
- * See the License for the specific language governing permissions and limitations under the License.
- */
-
-import Vue from 'vue'
-import Router from 'vue-router'
-import 'semantic-ui-css/semantic.min.css'
-
-import { OktaAuth } from '@okta/okta-auth-js'
-import OktaVue from '@okta/okta-vue'
-
+import { createRouter, createWebHistory } from 'vue-router'
+import { LoginCallback } from '@okta/okta-vue'
 import HomeComponent from '@/components/Home'
 import LoginComponent from '@/components/Login'
 import ProfileComponent from '@/components/Profile'
 import MessagesComponent from '@/components/Messages'
 
-import sampleConfig from '@/config'
-
-const oktaAuth = new OktaAuth(sampleConfig.oidc)
-
-Vue.use(Router)
-Vue.use(OktaVue, {
-  oktaAuth,
-  onAuthRequired: () => {
-    router.push('/login')
-  }
-})
-
-const router = new Router({
-  mode: 'history',
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
   routes: [
     {
       path: '/',
@@ -47,7 +18,7 @@ const router = new Router({
     },
     {
       path: '/login/callback',
-      component: LoginComponent
+      component: LoginCallback
     },
     {
       path: '/profile',
