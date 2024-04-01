e8f8ddc600404cf88f252f205562321c51ce0cdd
Jared Perreault <90656038+jaredperreault-okta@users.noreply.github.com>
2024-04-01 12:32:20 -0400

    vuln fix (#164)
    OKTA-680680 fixes ci

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,6 +7,7 @@ setup_service node v16.20.2
 # Revert the cache-min setting, since the internal cache does not apply to
 # these repos (and causes problems in lookups)
 npm config set cache-min 10
+npm config set registry https://registry.yarnpkg.com
 
 # Install dependences.
 if ! npm install --ignore-scripts; then
